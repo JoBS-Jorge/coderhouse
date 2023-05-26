671ef608ff519a0fd872e34e5f34699267a8bb0e
Jorge Braz de Souza Filho <jorge.not@gmail.com>
2023-05-26 11:25:18 -0300

    Desafio 1 - Coderhouse
    primeiro exercício do curso de javascript da coderhouse

---
+++ page.js
@@ -0,0 +1,12 @@
+let nome = prompt("Digite o seu nome.")
+alert(`Olá, ${nome}!`)
+let primeiroNumero = parseInt(prompt(`${nome}, digite um número.`))
+
+const numCinco = 5
+let soma = primeiroNumero + numCinco
+let subtracao = primeiroNumero - numCinco
+let multiplicacao = primeiroNumero * numCinco
+let divisao = primeiroNumero / numCinco
+
+alert(`Resultados do número que você digitou calculados com o número 5: Soma = ${soma} || Subtração = ${subtracao} || Multiplicação = ${multiplicacao} || Divisão = ${divisao}`)
+console.log(`Olá, ${nome} os resultados são: Soma = ${soma} || Subtração = ${subtracao} || Multiplicação = ${multiplicacao} || Divisão = ${divisao}`)
